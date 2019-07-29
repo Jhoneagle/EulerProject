10bf2917fa2c420b2b0ef656575b3bad7b5c899f
Jhoneagle <eaglekjohn@gmail.com>
2019-07-29 12:31:28 +0300

    14th implementation going

--- src/Power_digit_sum.cpp
+++ src/Power_digit_sum.cpp
@@ -14,12 +14,22 @@ typedef long long num;
 int main() {
   cin.sync_with_stdio(0);
   cin.tie(0);
-  vector<int> d;
-  int s = 0;
   
+  int max = 500, s = 0;
+  int ar[max];
+  ar[0] = 1;
   
+  for(int j = 1; j < max; j++) ar[j] = 0;
   
-  for (int i = 0; i < d.size(); i++) s += d[i];
+  for(int i = 0; i < 1000; i++) {
+    ar[0] = 2 * ar[0];
+    
+    for(int k = 1; k < max; k++) ar[k] = 2 * ar[k] + ar[k - 1] / 10;
+    
+    for(int j = 0; j < max; j++) ar[j] = ar[j] % 10;
+  }
+  
+  for(int i = 0; i < max; i++) s += ar[i];
   
   cout << s << "\n";
   return 0;
